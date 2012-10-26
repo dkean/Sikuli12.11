3dcb3287069afe83e513b6dda66b7a592118e9fd
Raimund Hocke <info@its-me-raiman.de>
2012-10-26 14:29:17 +0200

    added some convenience methods

--- Script-Java/org/sikuli/core/Location.java
+++ Script-Java/org/sikuli/core/Location.java
@@ -21,7 +21,12 @@ public class Location extends Point {
     super((int) x, (int) y);
   }
 
-  public Location(int x, int y) {
+  /**
+	 *
+	 * @param x
+	 * @param y
+	 */
+	public Location(int x, int y) {
     super(x, y);
   }
 
@@ -33,9 +38,23 @@ public class Location extends Point {
     super(point);
   }
 
-  public Location negative() {
-    return new Location(-x, -y);
-  }
+	public Point getPoint() {
+		return new Point(this);
+	}
+
+	public Screen getScreen() {
+		return Screen.getScreenContaining(this);
+	}
+
+	public Location moveFor(int X, int Y) {
+		super.translate(X, Y);
+		return this;
+	}
+
+	public Location moveTo(int X, int Y) {
+		super.move(X, Y);
+		return this;
+	}
 
   public Location offset(int dx, int dy) {
     return new Location(x + dx, y + dy);
@@ -56,6 +75,50 @@ public class Location extends Point {
   public Location below(int dy) {
     return new Location(x, y + dy);
   }
+
+	  /**
+   * new point with same offset to current screen's top left on primary screen
+   *
+   * @return new location
+   */
+  public Location copyTo() {
+    return copyTo(Screen.getPrimaryScreen());
+  }
+
+  /**
+   * new point with same offset to current screen's top left on given screen
+   *
+   * @param scrID number of screen
+   * @return new location
+   */
+  public Location copyTo(int scrID) {
+    return copyTo(Screen.getScreen(scrID));
+  }
+
+  /**
+   * new point with same offset to current screen's top left on given screen
+   *
+   * @param screen new parent screen
+   * @return new location
+   */
+  public Location copyTo(Screen screen) {
+    Location o = new Location(Screen.getScreenContaining(this).getBounds().getLocation());
+    Location n = new Location(screen.getBounds().getLocation());
+    return new Location(n.x + x - o.x, n.y + y - o.y);
+  }
+
+  /**
+   * new region with same offset to current screen's top left <br />on the given
+   * region's screen <br />mainly to support Jython Screen objects
+   *
+   * @param screen new parent screen
+   * @return new region
+   */
+  public Location copyTo(Region reg) {
+    return copyTo(reg.getScreen());
+  }
+
+
 
   @Override
   public String toString() {
