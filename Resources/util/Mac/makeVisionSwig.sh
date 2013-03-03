b05c6d69f9485d8655a96c556a32a0e4a0573df7
Raimund Hocke <rmhdevelop@me.com>
2013-03-03 10:45:58 +0100

    adapted swig command for VisionProxy project

--- Resources/util/Mac/makeVisionSwig.sh
+++ Resources/util/Mac/makeVisionSwig.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-base=`pwd`/../../$1
+base=`pwd`/../..
 odir=$base/Script-Java/org/sikuli/script/natives
 icv=/usr/local/include/opencv
-ivision=$base/Ressources/natives/Vision
+ivision=$base/Resources/natives/Vision
 sysJava=/System/Library/Frameworks/JavaVM.framework/Headers
 
 /usr/local/bin/swig -java -package org.sikuli.script.natives -outdir $odir -c++ -I$sysJava -I$icv/opencv -I$icv -I$ivision -I/usr/local/include -o $ivision/visionJAVA_wrap.cxx $ivision/vision.swig
