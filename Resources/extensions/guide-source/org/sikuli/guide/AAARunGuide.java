e43c85d7a126b5dcf74ea291ba3f848401a73e06
Raimund Hocke <rmhdevelop@me.com>
2013-03-22 14:55:59 +0100

    guide test

--- Resources/extensions/guide-source/org/sikuli/guide/AAARunGuide.java
+++ Resources/extensions/guide-source/org/sikuli/guide/AAARunGuide.java
@@ -4,6 +4,7 @@
  */
 package org.sikuli.guide;
 
+import java.awt.Color;
 import java.awt.Point;
 import org.sikuli.script.*;
 import java.awt.image.BufferedImage;
@@ -17,8 +18,14 @@ public class AAARunGuide {
   private static SikuliGuide g;
   private static SikuliGuideComponent ggc;
 
+  static SikuliGuideComponent.Layout LO = SikuliGuideComponent.Layout.OVER;
+  static SikuliGuideComponent.Layout LT = SikuliGuideComponent.Layout.TOP;
+  static SikuliGuideComponent.Layout LB = SikuliGuideComponent.Layout.BOTTOM;
+  static SikuliGuideComponent.Layout LL = SikuliGuideComponent.Layout.LEFT;
+  static SikuliGuideComponent.Layout LR = SikuliGuideComponent.Layout.RIGHT;
+  static Screen s = new Screen();
+
   public static void main(String[] args) throws FindFailed {
-    Screen s = new Screen();
     gTest(s);
   }
 
@@ -28,11 +35,13 @@ public class AAARunGuide {
     Region r2 = r1.grow(100);
 
     gR(r1);
-    gCo(r1, "Ein langer Text", 0);
+    gCo(r1, "Ein langer Text", 0, LB);
+    gCo(r1, "Ein langer Text", 0, LT);
     gC(r2);
-    gT(r2, "Ein Text", 24);
-    gB(r2, "ClickMe");
-    Point f = ggc.getLocation();
+    gCo(r1, "Ein sehr sehr langer langer langer Text", 36, LR);
+    gT(r2, "Ein Text", 36, LB);
+    gB(r2, "ClickMe", LL);
+    Point f = ggc.getRegion().getCenter();
     Point t = r2.getTopRight();
     gAr(f, t);
 
@@ -42,37 +51,39 @@ public class AAARunGuide {
 
   private static void gR(Region t) {
     SikuliGuideRectangle gc = new SikuliGuideRectangle(t);
-    gc.setLocationRelativeToRegion(t, SikuliGuideComponent.Layout.OVER);
+    gc.setLocationRelativeToRegion(t, LO);
     g.addToFront(gc);
     ggc = gc;
   }
 
   private static void gC(Region t) {
     SikuliGuideCircle gc = new SikuliGuideCircle(t);
-    gc.setLocationRelativeToRegion(t, SikuliGuideComponent.Layout.OVER);
+    gc.setLocationRelativeToRegion(t, LO);
     g.addToFront(gc);
     ggc = gc;
   }
 
-  private static void gB(Region t, String name) {
+  private static void gB(Region t, String name, SikuliGuideComponent.Layout l) {
     SikuliGuideButton gc = new SikuliGuideButton(name);
-    gc.setLocationRelativeToRegion(t, SikuliGuideComponent.Layout.LEFT);
+    gc.setLocationRelativeToRegion(t, l);
     g.addToFront(gc);
     ggc = gc;
   }
 
-  private static void gT(Region t, String text, int fSize) {
+  private static void gT(Region t, String text, int fSize, SikuliGuideComponent.Layout l) {
     SikuliGuideText gc = new SikuliGuideText(text);
     gc.setFontSize(fSize);
-    gc.setLocationRelativeToRegion(t, SikuliGuideComponent.Layout.BOTTOM);
+    gc.setLocationRelativeToRegion(t, l);
+    gc.updateComponent();
     g.addToFront(gc);
     ggc = gc;
   }
 
-  private static void gCo(Region t, String text, int fSize) {
+  private static void gCo(Region t, String text, int fSize, SikuliGuideComponent.Layout l) {
     SikuliGuideCallout gc = new SikuliGuideCallout(text);
-    //gc.setFontSize(fSize);
-    gc.setLocationRelativeToRegion(t, SikuliGuideComponent.Layout.RIGHT);
+    gc.setFontSize(fSize);
+    gc.setColors(null, null, Color.BLUE, null, null);
+    gc.setLocationRelativeToRegion(t, l);
     g.addToFront(gc);
     ggc = gc;
   }
