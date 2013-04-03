b8b5eecb40630f3ae46e54a13bf7989aab4e6ff2
Raimund Hocke <rmhdevelop@me.com>
2013-04-03 15:43:22 +0200

    Mac beta version: fixed issue with native libs

--- Resources/util/Mac/change-lib-path.sh
+++ Resources/util/Mac/change-lib-path.sh
@@ -5,11 +5,12 @@ echo ------------------ switching to @loader_path
 for dylib in $2*.dylib
 do
 	 echo ++++++++++---------------- $dylib
-#   otool -L $dylib
-#   echo -------------------- changes to make
+	 chmod ugo+rw $dylib
+   otool -L $dylib
+   echo -------------------- changes to make
    for ref in `otool -L $dylib | grep "lib.*.dylib[^:]" | awk '{print $1'} | grep -v '^/usr/lib' | grep -v '^/System/' | grep -v '^@loader'`
    do
-#      echo $ref
+      echo $ref
       install_name_tool -change $ref @loader_path/`basename $ref` $dylib
    done
    echo -------------------- changed version
